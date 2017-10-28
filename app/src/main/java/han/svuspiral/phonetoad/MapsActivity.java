7b548e6fee27b88cbdb5e14f82300d4099dbfe19
robotTree <hes09@yahoo.com>
2017-10-27 20:14:49 -0400

    functional splash screen transitions to functional map

--- app/src/main/java/han/svuspiral/phonetoad/MapsActivity.java
+++ app/src/main/java/han/svuspiral/phonetoad/MapsActivity.java
@@ -3,11 +3,11 @@ package han.svuspiral.phonetoad;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MapsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_maps);
     }
 }
