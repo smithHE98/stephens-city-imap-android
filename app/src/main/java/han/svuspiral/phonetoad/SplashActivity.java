ecf6bb62fa7007f138eec71ed6f31d5b17db5b39
Adrian Sanchez <adrian.sanchez@students.svu.edu>
2017-11-04 23:37:01 -0400

    milestone report 1: changed to sdk version 26 added app icon splash screen text & images correctly configured

--- app/src/main/java/han/svuspiral/phonetoad/SplashActivity.java
+++ app/src/main/java/han/svuspiral/phonetoad/SplashActivity.java
@@ -1,17 +1,35 @@
 package han.svuspiral.phonetoad;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class SplashActivity extends AppCompatActivity {
 
+    private TextView p1;
+    private TextView p2;
+    private TextView p3;
+    private ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+            p1 = (TextView)
+                    findViewById(R.id.p1);
+            p2 = (TextView)
+                    findViewById(R.id.p2);
+            imageView = (ImageView)
+                    findViewById(R.id.imageView);
+            p3 = (TextView)
+                    findViewById(R.id.p3);
         Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
         finish();
     }
+
 }
