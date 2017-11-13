f7d7dcbbe0b9a3d0f1dc60c9adf2c359faa6a725
robotTree <hes09@yahoo.com>
2017-11-13 12:57:19 -0500

    fixed logic errors in code, app runs and is completely functional set default view on map to Stephens City, not zoomed in yet added a placeholder marker in Stephens City SDK VERSION set to 24 to increase number of people able to use app

--- app/src/main/java/han/svuspiral/phonetoad/SplashActivity.java
+++ app/src/main/java/han/svuspiral/phonetoad/SplashActivity.java
@@ -10,26 +10,47 @@ import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private TextView p1;
-    private TextView p2;
-    private TextView p3;
-    private ImageView imageView;
+private TextView tv1;
+private TextView tv2;
+private TextView tv3;
+private ImageView iv;
+private Typeface ef, lf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-            p1 = (TextView)
-                    findViewById(R.id.p1);
-            p2 = (TextView)
-                    findViewById(R.id.p2);
-            imageView = (ImageView)
-                    findViewById(R.id.imageView);
-            p3 = (TextView)
-                    findViewById(R.id.p3);
-        Intent intent = new Intent(this, MapsActivity.class);
-        startActivity(intent);
-        finish();
+            tv1 = (TextView)
+                    findViewById(R.id.tv1);
+            tv2 = (TextView)
+                    findViewById(R.id.tv2);
+            iv = (ImageView)
+                    findViewById(R.id.iv);
+            tv3 = (TextView)
+                    findViewById(R.id.tv3);
+            ef = Typeface.createFromAsset(getAssets(), "font/Economica-Bold.ttf");
+            lf = Typeface.createFromAsset(getAssets(), "font/Lora-Regular.ttf");
+            tv1.setTypeface(ef);
+            tv2.setTypeface(ef);
+            tv3.setTypeface(lf);
+        final Intent i = new Intent(this, MapsActivity.class);
+        Thread timer = new Thread() {
+            public void run () {
+                try {
+                    sleep(2500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(i);
+                    finish();
+
+                }
+            }
+        };
+        timer.start();
+
+
+    }
     }
 
-}
