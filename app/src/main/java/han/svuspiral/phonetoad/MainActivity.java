57dcf62b31e4e53f5fb90f4148933459c6d43fcd
robotTree <hes09@yahoo.com>
2018-07-13 21:56:49 -0400

    json parsing needed + json parser class added - unnecessary file removed

--- app/src/main/java/han/svuspiral/phonetoad/MainActivity.java
+++ app/src/main/java/han/svuspiral/phonetoad/MainActivity.java
@@ -3,7 +3,11 @@ package han.svuspiral.phonetoad;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     android.support.v4.app.FragmentTransaction fragmentTransaction;
 
